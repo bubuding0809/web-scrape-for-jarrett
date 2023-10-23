9719dcb49e351c357fa407793ff71170126a5399
bubuding0809 <bubuding0809@gmail.com>
2023-10-24 00:42:23 +0800

    Initial setup

---
+++ main.py
@@ -0,0 +1,77 @@
+import os
+import aiohttp
+import asyncio
+import pandas as pd
+from io import StringIO
+from bs4 import BeautifulSoup
+
+
+async def fetch_data(symbol, session):
+    url = f"https://www.alphaquery.com/stock/{symbol}/volatility-option-statistics/90-day/historical-volatility"
+
+    async with session.get(url) as response:
+        if response.status != 200:
+            print(f"Error fetching data for {symbol}")
+            return None
+
+        html = await response.text()
+        soup = BeautifulSoup(html, "html.parser")
+        table = soup.find("table")
+
+        if not table:
+            print(f"Could not find table for {symbol}")
+            return None
+
+        # Wrap the HTML string in a StringIO object
+        html_string_io = StringIO(str(table))
+        df = pd.read_html(html_string_io)[0]
+
+        # Iterate through each row in the DataFrame
+        row_object = {
+            "Symbol": symbol,
+        }
+        for _, row in df.iterrows():
+            try:
+                row_object[row[0]] = float(row[1])
+            except ValueError:
+                row_object[row[0]] = row[1]
+        row_object = dict(
+            filter(
+                lambda x: x[0] not in ["Volatility Metrics", "Option Statistics"],
+                row_object.items(),
+            )
+        )
+
+        # Create a new DataFrame from the row_object
+        df = pd.DataFrame([row_object])
+
+        print(f"Scraped data for {symbol}")
+        return df
+
+
+async def main():
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch_data(symbol, session) for symbol in stock_symbols]
+        results = await asyncio.gather(*tasks)
+
+    print(f"Scraping complete for {len(results)} symbols")
+
+    # Combine the data for all symbols into a single DataFrame
+    combined_data = pd.concat(
+        [df for df in results if df is not None], ignore_index=True
+    )
+
+    # Write the data to an Excel file called AlphaQuery.xlsx
+    with pd.ExcelWriter("AlphaQuery.xlsx") as writer:
+        combined_data.to_excel(writer, index=False)
+
+
+if __name__ == "__main__":
+    # Read the Excel file and extract the stock symbols from the Stock List tab
+    # Set current working directory to the directory of this file
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
+    path = input("Enter the path to the Excel file: ")
+    df = pd.read_excel(path, sheet_name="Stock List")
+    stock_symbols = df["Symbol"].tolist()
+
+    asyncio.run(main())
